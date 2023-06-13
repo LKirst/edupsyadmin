5960d3768989e414aca1e0bd49e8db4b09292fcc
LKirst <lk_tech@mailbox.org>
2023-06-13 10:25:24 +0200

    Add function to create an lrst table

---
+++ src/edupsy_admin/core/create_tbl_lrst.py
@@ -0,0 +1,32 @@
+import datetime
+from create_tbl_names import get_names
+from utils_sql import get_sql_con, get_tbl_info
+
+
+def session_tbl(con, fn):
+    """Create a table for tests and questionnaires.
+
+    Parameters
+    ----------
+    con : _type_
+        _description_
+    fn : function
+        _description_
+    """
+    names = get_names(fn)
+    query_string = (
+        "CREATE TABLE lrst("
+        "diagnose text NOT NULL, "
+        "na_zeitzuschlag int NOT NULL, "
+        "na_aufgabevorlesen bool NOT NULL, "
+        "ns_rs bool NOT NULL, "
+        "ns_gewichtungmdl bool NOT NULL, "
+        "ns_nichtvorlesen bool NOT NULL, "
+        "datetime_created text NOT NULL, "
+        "datetime_lastmodified text NOT NULL, "
+        "client_id TEXT NOT NULL, "
+    )
+    query_string += "PRIMARY KEY (datetime_created, client_id));"
+    cur = con.cursor()
+    cur.execute(query_string)
+    con.commit()
