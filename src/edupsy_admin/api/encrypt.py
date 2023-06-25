7e64c8272d49fc2f49a95b96c3fa520f6442bf12
LKirst <lk_tech@mailbox.org>
2023-06-25 11:26:55 +0200

    Add keyring

--- src/edupsy_admin/api/encrypt.py
+++ src/edupsy_admin/api/encrypt.py
@@ -1,25 +1,32 @@
 import base64
 import os
+import keyring
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+def fernet_from_pw(username: str):
+    """Use a password with Fernet to derive a key
+    (see https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet)
+    """
+    password=keyring.get_password("edupsy", username)
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    return f
 
-# Use a password with Fernet to derive a key
-# see https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
-password = b"password"
-salt = os.urandom(16)
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=480000,
-)
-key = base64.urlsafe_b64encode(kdf.derive(password))
-f = Fernet(key)
+def encrypt(data: bytes, username: str):
+    f = fernet_from_pw(username)
+    token = f.encrypt(data)
+    return token
 
-# encrypt a secret
-token = f.encrypt(b"Secret message!")
-
-# decrypt a secret
-f.decrypt(token)
+def decrypt(token: bytes, username: str):
+    f = fernet_from_pw(username)
+    data=f.decrypt(token)
+    return data
