6dc51e5bfef424274462005315a66848f57ac54e
LKirst <lk_tech@mailbox.org>
2023-08-29 13:02:25 +0200

    Move the categories in a data file

--- src/edupsy_admin/api/taetigkeitsbericht_entry_interactive.py
+++ src/edupsy_admin/api/taetigkeitsbericht_entry_interactive.py
@@ -1,55 +1,13 @@
+from importlib.resources import files
 import pandas as pd
 
-
-possible_keywords = {
-        'slbb.slb.wchslbundesl', # B. z. Schullaufbahn: Wechsel aus anderem Bundesland
-        'slbb.slb.wchslstaat', # B. z. Schullaufbahn: Wechsel aus einem anderen Bundesland
-        'slbb.slb.sonstige', # B. z. Schullaufbahn: Schullwahl, Wechsel, Abschlüsse
-        'slbb.einsch', # Einschulung
-        'slbb.uebertweiterf', # Übertritt an weiterführende Schulen
-        'slbb.berufustudienw', # Berufs- und Studienwahlorientierung
-        'slbb.sonstiges', # Schullaufbahnberatung: Sonstiges
-        'ppb.besbeg', # Besondere Begabungen
-        'ppb.inkl', # Inklusion
-        'ppb.integrmigr', # Integration / Migration
-        'ppb.lrst', # LRST Beratung
-        'sl.lrststllngn.erstm', # Erst. einer schulpsy. Stellungn.: erstmalig
-        'sl.lrststllngn.erneut', # Erst. einer schulpsy. Stellungn.: erneut
-        'sl.lrststllngn.facharzt', # Erst. einer schulpsy. Stellungn.: aufgr. fachärztl. Zeugnisses
-        'sl.dyskalkulie', # Dyskalkulie
-        'sl.sonstige', # Weitere Lern- und Leistungsprobleme
-        'schulabsentismus', # Schulabsentismus
-        'subkl.a.pruefungs',
-        'subkl.a.schulangst',
-        'subkl.allgverhaltenspr',
-        'subkl.gewaltaggr',
-        'subkl.mobbing',
-        'geschlechterdys',
-        'konfl.peer',
-        'pubertaet',
-        'kl.angstst',
-        'kl.depr',
-        'kl.essst',
-        'kl.sucht',
-        'kl.suiz',
-        'kl.trauma',
-        'kl.zwang',
-        'kl.adhs',
-        'kl.autism',
-        'kl.sonst',
-        'chronischeerkr',
-        'konfl.lehrkr',
-        'sonstiges'
-        }
-
-
-def validate_taetigkeitsbericht_keyword(keyword:str) -> bool:
-    return keyword in possible_keywords
+def get_taet_categories():
+    categoryfile = files('edupsy_admin.data').joinpath('taetigkeitsbericht_categories_SJ202223.ods')
+    return pd.read_excel(categoryfile)
 
 def askyn(prompt):
     yes = {"yes", "ye", "y"}
     no = {"no", "n"}
-
     answ = input(prompt).lower()
     if answ in yes:
         return 1
@@ -57,22 +15,27 @@ def askyn(prompt):
         return 0
     else:
         print('Only "y" or "n" are allowed as responses.')
-        return askyn(prompt)
+        return askyn(prompt) # ask recursively until the answer is correct
 
-if __name__ == "__main__":
+def interactive_collect_taet(categories):
+    possible_keywords = set(categories)
     data = []
     while askyn(f'Continue (y|n)? '):
+        school=input('school: ')
         client_id=input('client_id: ')
-        last_name=input('last_name: ')
         keyword=input('keyword: ')
         while keyword not in possible_keywords:
             keyword=input('keyword (previous answer is not an option): ')
         nsitzungen=input('nsitzungen: ')
         data.append([
+            school,
             client_id,
-            last_name,
             keyword,
             nsitzungen
             ])
     df = pd.DataFrame(data)
     df.to_csv('archivierung.csv')
+
+if __name__ == "__main__":
+    df = get_taet_categories()
+    interactive_collect_taet(df['taetkey'])
