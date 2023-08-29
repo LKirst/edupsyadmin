cee7ceaea00ba30a63353af9bde4136ac2c4baf8
LKirst <lk_tech@mailbox.org>
2023-08-29 15:18:05 +0200

    Create pdf report

--- src/edupsy_admin/api/taetigkeitsbericht_analysis.py
+++ src/edupsy_admin/api/taetigkeitsbericht_analysis.py
@@ -1,10 +1,14 @@
 import os
 import re
 import argparse
+import logging
+from datetime import date
+
 import pandas as pd
 import numpy as np
-import logging
 
+import dataframe_image as dfi
+from fpdf import FPDF
 
 WD_WEEK = 5 # workdays per week
 WD_YEAR = 251-30 # workdays per year
@@ -12,6 +16,26 @@ WW_YEAR = WD_YEAR / WD_WEEK # work weeks per year
 ZSTD_WEEK = 40 # Zeitstunden per work week
 ZSTD_DAY = ZSTD_WEEK / WD_WEEK # Zeitstunden per work day
 ZSTD_YEAR = ZSTD_DAY * WD_YEAR # Zeitstunden per year
+
+
+class Report(FPDF):
+    def __init__(self):
+        super().__init__()
+        self.WIDTH = 210
+        self.HEIGHT = 297
+        self.header_text = f'Tätigkeitsbericht {date.today()}'
+
+    def header(self):
+        self.set_font("Arial", "B", 11)
+        self.cell(w=0, h=10, txt=self.header_text, border=0, ln=0, align="C")
+        self.ln(20)
+
+    def footer(self):
+        # page numbers
+        self.set_y(-15)
+        self.set_font("Arial", "I", 8)
+        self.set_text_color(128)
+        self.cell(0, 10, "Page " + str(self.page_no()), border=0, ln=0, align="C")
 
 
 def read_csv(*args) -> pd.DataFrame:
@@ -92,7 +116,7 @@ def summary_statistics_wstd(
         wstd_total: int,
         zstd_spsy_year_actual: float,
         *schools:str
-        ) -> dict:
+        ) -> pd.Series:
     """Calculate Wochenstunden summary statistics
 
     Parameters
@@ -109,7 +133,7 @@ def summary_statistics_wstd(
 
     Returns
     -------
-    dict
+    pd.Series
         Wochenstunden summary statistics
     """
     summarystats_wstd = wstd_in_zstd(wstd_spsy,wstd_total)
@@ -131,7 +155,50 @@ def summary_statistics_wstd(
         summarystats_wstd['perc_spsy_year_actual'] = (
             zstd_spsy_year_actual/summarystats_wstd['zstd_spsy_year_target']
             )*100
-    return summarystats_wstd
+    return pd.Series(summarystats_wstd)
+
+def create_taetigkeitsbericht_report(
+        basename_out:str,
+        name:str,
+        summary_wstd: pd.Series,
+        summary_categories: pd.DataFrame,
+        summary_nsitzungen: pd.DataFrame
+        ) -> None:
+
+    if not os.path.exists('resources'):
+        os.makedirs('resources')
+    wstd_img = 'resources/summary_wstd.png'
+    dfi.export(summarystats_wstd.to_frame(name='values'),
+            wstd_img,
+            table_conversion='matplotlib')
+    nsitzungen_img='resources/summary_nsitzungen.png'
+    dfi.export(summary_nsitzungen, nsitzungen_img,
+            table_conversion='matplotlib')
+
+    categorycols=summary_categories.columns
+    chunks = [categorycols[x:x+8] for x in range(0, len(categorycols), 8)]
+
+    report = Report()
+    report.add_page()
+    for nm, val in summary_categories.items():
+        report.cell(w=15, h=9, border=0, txt=f"{nm}:")
+        report.ln(6)
+        for text in [
+                "einmaliger Kurzkontakt",
+                "1-3 Sitzungen",
+                "mehr als 3 Sitzungen"]:
+            report.cell(w=50, h=9, border=0, txt=text)
+        report.ln(6) # linebreak
+        for colnm in [
+                'count_einm_kurzkont',
+                'count_1to3_sessions',
+                'count_mt3_sessions']:
+            report.cell(w=50, h=9, border=0,txt=f"{val[colnm]:.0f}")
+        report.ln(18) # linebreak
+    report.add_page()
+    report.image(nsitzungen_img, x=15, y=report.HEIGHT * 1/4, w=180)
+    report.image(wstd_img, x=15, y=report.HEIGHT * 1/2, w=90)
+    report.output(basename_out+'_report.pdf')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -146,6 +213,8 @@ if __name__ == "__main__":
     parser.add_argument('--wstd_total', type=int, default=23,
             help="total Wochstunden (depends on your school)")
     parser.add_argument('--csvfiles', nargs='+', type=str, help='list of files')
+    parser.add_argument('--name', type=str, default='Schulpsychologie',
+        help='Name for the subheading of the pdf report')
     args = parser.parse_args()
 
     if args.csvfiles is not None:
@@ -169,11 +238,19 @@ if __name__ == "__main__":
 
 
     # Summary statistics for Wochenstunden
-    summarystats_wstd = pd.Series(summary_statistics_wstd(
+    summarystats_wstd = summary_statistics_wstd(
             args.wstd_psy,
             args.wstd_total,
             zstd_spsy_year_actual,
             *args.nstudents
-            ))
+            )
     summarystats_wstd.to_csv(args.out_basename + '_wstd.csv')
     print(summarystats_wstd)
+
+    create_taetigkeitsbericht_report(
+            args.out_basename,
+            args.name,
+            summarystats_wstd,
+            summary_categories,
+            summarystats_nsitzungen
+            )
