5f791fe734f55b32ac695a79864459de34ac7a80
LKirst <lk_tech@mailbox.org>
2023-10-13 07:32:24 +0200

    Flatten Mantelbogen

--- shellscripts/mantelbogen.sh
+++ shellscripts/mantelbogen.sh
@@ -1,2 +1,4 @@
 edupsy_admin -w DEBUG create_documentation lukas.liebermann $1\
     ~/Templates/Beratung/LK_Mantelbogen_2023.pdf
+
+python ~/bin/libadmin/pdf_two_pages_per_sheet.py -p "$1_LK_Mantelbogen_2023.pdf" --flatten
