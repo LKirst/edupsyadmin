904e21f95f3d8500c46758895268b8a9e1826261
LKirst <lk_tech@mailbox.org>
2023-10-17 15:23:41 +0200

    Set the config path in the shell script

--- shellscripts/mantelbogen.sh
+++ shellscripts/mantelbogen.sh
@@ -1,4 +1,5 @@
-edupsy_admin -w DEBUG create_documentation lukas.liebermann $1\
+edupsy_admin -w DEBUG -c ~/bin/edupsy_admin/etc/config.yml\
+    create_documentation lukas.liebermann $1\
     ~/Templates/Beratung/LK_Mantelbogen_2023.pdf
 
 python ~/bin/libadmin/pdf_two_pages_per_sheet.py -p "$1_LK_Mantelbogen_2023.pdf" --flatten
