fe54ed22a9271af2fb2e1137d52658398504741f
Lukas Liebermann <lk_tech@mailbox.org>
2023-10-17 20:52:08 +0200

    Add some convenience scripts

---
+++ shellscripts/AddToDB.sh
@@ -0,0 +1,3 @@
+csv_input=$1
+
+edupsy_admin -c ~/bin/edupsy_admin/etc/config.yml new_client lukas.liebermann --csv $csv_input
---
+++ shellscripts/ExtractStudentFromCsv.sh
@@ -0,0 +1,5 @@
+student=$1
+csv_input=$2
+
+csv_output="s_$student.csv"
+in2csv $csv_input -t | csvgrep -c name -m $student > $csv_output
