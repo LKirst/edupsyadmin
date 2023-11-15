23715a11611d66a5c03bb13f8ec30c57338d3f65
LKirst <lk_tech@mailbox.org>
2023-11-15 07:19:04 +0100

    Add documentation shell script for bsmn

---
+++ shellscripts/lrst_documentation_bsmn.sh
@@ -0,0 +1,25 @@
+diagnosis=1
+while getopts "n" flag; do
+case "$flag" in
+    n) diagnosis=0;;
+esac
+done
+
+client_id=${@:$OPTIND:1}
+
+if [ $diagnosis -eq 1 ]
+then
+edupsy_admin -w WARN -c ~/bin/edupsy_admin/etc/config.yml\
+    create_documentation lukas.liebermann $client_id\
+    ~/Templates/Beratung/LRSt_GenehmigungSchulleitung_2023_bsmn.pdf\
+    ~/Templates/Beratung/LRSt_Stellungnahme_2023_bsmn.pdf\
+    ~/Templates/Beratung/LRSt_Informationsschreiben_2023.pdf\
+    ~/Templates/Beratung/LRSt_Anschreiben_2023.pdf\
+    ~/Templates/Beratung/LRSt_AntragNachUeberpruefung_2023_bsmn.pdf
+else
+edupsy_admin -w WARN -c ~/bin/edupsy_admin/etc/config.yml\
+    create_documentation lukas.liebermann $client_id\
+    ~/Templates/Beratung/LRSt_StellungnahmeKeinBefund_2023.pdf\
+    ~/Templates/Beratung/LRSt_AnschreibenKeinBefund_2023.pdf
+fi
+
