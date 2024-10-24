359440e60026497f4fb19989c8128541e46758a3
Lehrer <lk_tech@mailbox.org>
2024-10-24 12:01:15 +0200

    refactor to move add_convenience data to its own file

---
+++ src/edupsy_admin/api/add_convenience_data.py
@@ -0,0 +1,64 @@
+from datetime import date, datetime
+from dateutil.parser import parse
+
+from ..core.logger import logger
+from ..core.config import config
+
+def add_convenience_data(data: dict) -> dict:
+    """Add the information which can be generated from existing key value pairs.
+
+    Parameters
+    ----------
+    data : dict
+        A dictionary of data values. Must contain "last_name", "first_name",
+        "street", "city".
+    """
+    data["name"] = data["first_name"] + " " + data["last_name"]
+
+    try:
+        data["address"] = data["street"] + ", " + data["city"]
+        data["address_multiline"] = (
+            data["name"] + "\n" + data["street"] + "\n" + data["city"]
+        )
+    except:
+        logger.debug("Couldn't add home address.")
+
+    # school address
+    schoolconfig = config.school[data["school"]]
+    data["school_name"] = schoolconfig["school_name"]
+    data["school_street"] = schoolconfig["school_street"]
+    data["school_head_w_school"] = schoolconfig["school_head_w_school"]
+
+    # Notenschutz and Nachteilsausgleich
+    if data["nachteilsausgleich"] or data["notenschutz"]:
+        school_subjects = get_subjects(data["school"])
+        logger.debug(f"\nsubjects:\n{school_subjects}")
+    if data["notenschutz"]:
+        data["ns_subjects"] = school_subjects
+        data["ns_zeugnisbemerkung"] = (
+            "Auf die Bewertung der Rechtschreibleistung wurde verzichtet."
+        )
+        data["ns_measures"] = "Verzicht auf die Bewertung der Rechtschreibleistung"
+    if data["nachteilsausgleich"]:
+        data["na_subjects"] = school_subjects
+        data["na_measures"] = (
+            f"Verlängerung der Arbeitszeit um {data['nta_sprachen']}% "
+            "bei schriftlichen Leistungsnachweisen und der "
+            "Vorbereitungszeit bei mündlichen Leistungsnachweisen"
+        )
+
+    # for forms, I use the format dd/mm/YYYY; internally, I use YYYY-mm-dd
+    today = date.today()
+    data["date_today"] = today.strftime("%d/%m/%Y")
+    try:
+        data["birthday"] = parse(data["birthday"], dayfirst=False).strftime("%d/%m/%Y")
+    except:
+        logger.error("The birthday could not be parsed.")
+        data["birthday"] = ""
+    data["school_year"] = get_this_academic_year_string()
+    data["document_shredding_date"] = data["document_shredding_date"].strftime(
+        "%d/%m/%Y"
+    )
+
+    return data
+
--- src/edupsy_admin/api/fill_form.py
+++ src/edupsy_admin/api/fill_form.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
 import argparse
 from itertools import product
 from pypdf import PdfReader, PdfWriter
 from pathlib import Path
-from datetime import date, datetime
-from dateutil.parser import parse
 import pandas as pd
 import shutil
 from importlib.resources import files
@@ -13,9 +10,9 @@ from liquid import Template, exceptions
 from fillpdf import fillpdfs
 
 from .academic_year import get_this_academic_year_string
+from .add_convenience_data import add_convenience_data
 from ..core.logger import logger
 from ..core.encrypt import Encryption
-from ..core.config import config
 
 
 def get_subjects(school: str):
@@ -28,66 +25,6 @@ def get_subjects(school: str):
     else:
         logger.warning(f"school subjects file does not exist!")
         return ""
-
-
-def add_convenience_data(data: dict) -> dict:
-    """Add the information which can be generated from existing key value pairs.
-
-    Parameters
-    ----------
-    data : dict
-        A dictionary of data values. Must contain "last_name", "first_name",
-        "street", "city".
-    """
-    data["name"] = data["first_name"] + " " + data["last_name"]
-
-    try:
-        data["address"] = data["street"] + ", " + data["city"]
-        data["address_multiline"] = (
-            data["name"] + "\n" + data["street"] + "\n" + data["city"]
-        )
-    except:
-        logger.debug("Couldn't add home address.")
-
-    # school address
-    schoolconfig = config.school[data["school"]]
-    data["school_name"] = schoolconfig["school_name"]
-    data["school_street"] = schoolconfig["school_street"]
-    data["school_head_w_school"] = schoolconfig["school_head_w_school"]
-
-    # Notenschutz and Nachteilsausgleich
-    if data["nachteilsausgleich"] or data["notenschutz"]:
-        school_subjects = get_subjects(data["school"])
-        logger.debug(f"\nsubjects:\n{school_subjects}")
-    if data["notenschutz"]:
-        data["ns_subjects"] = school_subjects
-        data["ns_zeugnisbemerkung"] = (
-            "Auf die Bewertung der Rechtschreibleistung wurde verzichtet."
-        )
-        data["ns_measures"] = "Verzicht auf die Bewertung der Rechtschreibleistung"
-    if data["nachteilsausgleich"]:
-        data["na_subjects"] = school_subjects
-        data["na_measures"] = (
-            f"Verlängerung der Arbeitszeit um {data['nta_sprachen']}% "
-            "bei schriftlichen Leistungsnachweisen und der "
-            "Vorbereitungszeit bei mündlichen Leistungsnachweisen"
-        )
-
-    # for forms, I use the format dd/mm/YYYY; internally, I use YYYY-mm-dd
-    today = date.today()
-    data["date_today"] = today.strftime("%d/%m/%Y")
-    try:
-        data["birthday"] = parse(data["birthday"], dayfirst=False).strftime("%d/%m/%Y")
-    except:
-        logger.error("The birthday could not be parsed.")
-        data["birthday"] = ""
-    data["school_year"] = get_this_academic_year_string()
-    data["document_shredding_date"] = data["document_shredding_date"].strftime(
-        "%d/%m/%Y"
-    )
-
-    return data
-
 
 def write_form_pdf(fn, out_fn, data, verbose=False):
     """uses pypdf"""
