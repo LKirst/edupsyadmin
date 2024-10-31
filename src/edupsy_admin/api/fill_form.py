3c8f9f735e91ad014a2fed3557de11f74c717dc6
Lukas Liebermann <lk_tech@mailbox.org>
2024-10-31 11:56:22 +0100

    add docstrings

--- src/edupsy_admin/api/fill_form.py
+++ src/edupsy_admin/api/fill_form.py
@@ -1,17 +1,24 @@
 from itertools import product
-from pypdf import PdfReader, PdfWriter
 from pathlib import Path
 import shutil
 
 from liquid import Template, exceptions
-from fillpdf import fillpdfs
 
 from .add_convenience_data import add_convenience_data
 from ..core.logger import logger
 
 
-def write_form_pdf(fn: Path, out_fn: Path, data: dict, verbose=False):
-    """uses pypdf"""
+def write_form_pdf(fn: Path, out_fn: Path, data: dict) -> None:
+    """
+    Fill a pdf form with data using pypdf.
+
+    :param fn: filename of the empty pdf form
+    :param out_fn: filename for the output
+    :param data: the data to fill the pdf with
+    :raises FileExistsError: FileExistsError
+    """
+    from pypdf import PdfReader, PdfWriter
+
     reader = PdfReader(open(fn, "rb"), strict=False)
     writer = PdfWriter()
 
@@ -40,8 +47,16 @@ def write_form_pdf(fn: Path, out_fn: Path, data: dict, verbose=False):
         writer.write(output_stream)
 
 
-def write_form_pdf2(fn: Path, out_fn: Path, data: dict, verbose=False):
-    """uses the library fillpdf"""
+def write_form_pdf2(fn: Path, out_fn: Path, data: dict) -> None:
+    """
+    Fill a pdf form with data using fillpdf.
+
+    :param fn: filename of the empty pdf form
+    :param out_fn: filename for the output
+    :param data: the data to fill the pdf with
+    """
+    from fillpdf import fillpdfs
+
     fields = fillpdfs.get_form_fields(fn)
     logger.debug(f"\nForm fields:\n{fields}")
     logger.debug(f"\nData keys:\n{data.keys()}")
@@ -57,18 +72,26 @@ def write_form_pdf2(fn: Path, out_fn: Path, data: dict, verbose=False):
         shutil.copyfile(fn, out_fn)
 
 
-def write_form_md(fn: Path, out_fn: Path, data):
+def write_form_md(fn: Path, out_fn: Path, data: dict) -> None:
+    """
+    Render a liquid template with data passed to the function.
+
+    :param fn: filename of a text file with the liquid template
+    :param out_fn: filename for the output
+    :param data: the data to fill the liquid template with
+    :raises [TODO:name]: [TODO:description]
+    """
     with open(fn, "r", encoding="utf8") as text_file:
         txt = text_file.read()
         try:
             template = Template(txt)
         except exceptions.Error as e:
-            print(txt)
+            logger.error(txt)
             raise e
         try:
             msg = template.render(**data)
         except exceptions.Error as e:
-            print(e)
+            logging.error(e)
             msg = ""
     with open(out_fn, "w", encoding="utf8") as out_file:
         out_file.writelines(msg)
@@ -78,8 +101,18 @@ def fill_form(
     client_data: dict,
     form_paths: list[str],
     use_fillpdf: bool = True,
-    verbose: bool = False,
 ):
+    """
+    A wrapper function for different functions to fill out forms and
+    templates based on client data.
+
+    :param client_data: value key pairs where the key is the name of the form
+        field or liquid variable
+    :param form_paths: a list of paths to pdf forms or liquid templates
+    :param use_fillpdf: there are two options for pdf-forms - either a function
+        that uses the library fillpdf or a function that uses pypdf2, defaults
+        to True
+    """
     data = add_convenience_data(client_data)
     for fn in form_paths:
         fp = Path(fn)
@@ -89,6 +122,6 @@ def fill_form(
         if fp.suffix == ".md":
             write_form_md(fp, out_fp, data)
         elif use_fillpdf:
-            write_form_pdf2(fp, out_fp, data, verbose=verbose)
+            write_form_pdf2(fp, out_fp, data)
         else:
-            write_form_pdf(fp, out_fp, data, verbose=verbose)
+            write_form_pdf(fp, out_fp, data)
