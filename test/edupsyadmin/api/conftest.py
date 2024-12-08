725b0f9bd599ea1e71a64e2d0c4c3f19a92cd865
Lukas Liebermann <lk_tech@mailbox.org>
2024-12-08 07:21:20 +0100

    refactor: move code for creation of sample pdf to separate file

--- test/edupsyadmin/api/conftest.py
+++ test/edupsyadmin/api/conftest.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
 import pytest
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas
+from sample_pdf_form import create_pdf_form
 
 from edupsyadmin.core.config import config
 from edupsyadmin.core.logger import logger
@@ -62,78 +60,6 @@ def mock_webuntis(tmp_path):
     webuntis_path.write_text(webuntis_content.strip())
     print(f"webuntis_path: {webuntis_path}")
     yield webuntis_path
-
-
-def create_pdf_form(pdf_filename):
-    c = canvas.Canvas(pdf_filename, pagesize=A4)
-    page_width, page_height = A4
-
-    # a textfield widget
-    c.drawString(100, 740, "first_name:")
-    c.acroForm.textfield(
-        name="first_name",
-        tooltip="Enter text here",
-        x=100,
-        y=700,
-        width=400,
-        height=30,
-        borderColor=colors.black,
-        fillColor=colors.white,
-        textColor=colors.black,
-        forceBorder=True,
-        maxlen=100,
-        value="",
-    )
-
-    # two checkbox widgets (the value is either YES or OFF)
-    c.drawString(130, 650, "notenschutz")
-    c.acroForm.checkbox(
-        name="notenschutz",
-        x=100,
-        y=650,
-        size=20,
-        borderWidth=3,
-        borderColor=colors.black,
-    )
-    c.drawString(130, 550, "nachteilsausgleich")
-    c.acroForm.checkbox(
-        name="nachteilsausgleich",
-        x=100,
-        y=550,
-        size=20,
-        borderWidth=3,
-        borderColor=colors.black,
-    )
-
-    # Radio buttons for gender selection
-    # TODO: test whether the correct value was set
-    c.drawString(100, 500, "Gender:")
-    c.acroForm.radio(
-        name="gender",
-        value="f",
-        x=100,
-        y=480,
-        size=20,
-        borderWidth=1,
-        borderColor=colors.black,
-        fillColor=colors.white,
-        forceBorder=True,
-    )
-    c.drawString(130, 480, "f")
-    c.acroForm.radio(
-        name="gender",
-        value="m",
-        x=100,
-        y=450,
-        size=20,
-        borderWidth=1,
-        borderColor=colors.black,
-        fillColor=colors.white,
-        forceBorder=True,
-    )
-    c.drawString(130, 450, "m")
-
-    c.save()
 
 
 @pytest.fixture
