4150a55ff92fd219e34f1578d995a439466f30ee
Lukas Liebermann <lk_tech@mailbox.org>
2025-03-04 09:52:51 +0100

    fix(taetigkeitsbericht_check_key.py): fix the path to the taetigkeitsbericht_categories file

--- src/edupsyadmin/api/taetigkeitsbericht_check_key.py
+++ src/edupsyadmin/api/taetigkeitsbericht_check_key.py
@@ -7,7 +7,7 @@ from ..core.logger import logger
 
 def get_taet_categories():
     categoryfile = files("edupsyadmin.data").joinpath(
-        "taetigkeitsbericht_categories_SJ202425.ods"
+        "taetigkeitsbericht_categories.csv"
     )
     categories = pd.read_csv(categoryfile)["taetkey"]
     return set(categories)
